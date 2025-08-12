d1b8482b0bdbd754e7e9108720029737c5a43394
Steve Nahrup <snahrup@gmail.com>
2025-08-12 09:54:48 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
           className="hidden bg-gradient-to-b from-neutral-500/10 to-neutral-500/0 bg-clip-text text-[10rem] leading-none font-black text-transparent select-none sm:block lg:text-[16rem]"
           style={{ marginBottom: '-2.5rem' }}
         >
-          Toukoum
+          Nahrup
         </div>
       </div>
 
@@ -99,7 +99,7 @@ export default function Home() {
           animationDuration={1.5}
           label="Star"
           size={'sm'}
-          repoUrl="https://github.com/toukoum/portfolio"
+          repoUrl="https://github.com/snahrup/portfolio"
         />
       </div>
 
